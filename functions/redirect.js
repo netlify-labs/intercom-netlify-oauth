33f6c9ed788c6d186c06af152ed751321b338bf3
DavidWells <hello@davidwells.io>
2018-06-28 16:41:56 -0700

    change other test redirect function

--- functions/redirect.js
+++ functions/redirect.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
    const response = {
     statusCode: 301,
     headers: {
-      Location: 'https://netlify.com',
+      Location: 'https://lol.com',
     }
   };
 
