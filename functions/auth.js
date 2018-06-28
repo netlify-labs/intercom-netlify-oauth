3a0894b8c58e1c44bd176c20d3500286db952f07
DavidWells <hello@davidwells.io>
2018-06-28 15:43:38 -0700

    update function plz

--- functions/auth.js
+++ functions/auth.js
@@ -1,18 +1,16 @@
 import oauth2, { config } from './utils/oauth'
 
-console.log('oauth2', oauth2)
-
-// Authorization uri definition
-var authorization_uri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: config.redirect_uri,
-  scope: '',
-  state: ''
-});
-
 exports.handler = (event, context, callback) => {
-  console.log('change this', authorization_uri)
-  // authorization_uri
-   const response = {
+  // Authorization uri definition
+  const authorization_uri = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: config.redirect_uri,
+    scope: '',
+    state: ''
+  });
+  console.log('authorization_uri', authorization_uri)
+  
+  /* redirect user to intercom login */
+  const response = {
     statusCode: 301,
     headers: {
       Location: authorization_uri,
