62f84793aa9b78bdf95dcb0f356fd87dae34f47b
DavidWells <hello@davidwells.io>
2018-06-28 14:54:11 -0700

    update auth

--- functions/utils/oauth.js
+++ functions/utils/oauth.js
@@ -1,21 +1,26 @@
 import simpleOauth from 'simple-oauth2' 
 
 export const config = {
+  app_id: process.env.INTERCOM_APP_ID,
   clientSecret: process.env.INTERCOM_CLIENT_SECRET,
   clientID: process.env.INTERCOM_CLIENT_ID,
   authorizationPath: 'https://app.intercom.io/oauth',
   tokenPath: 'https://api.intercom.io/auth/eagle/token',
   profilePath: 'https://api.intercom.io/me/',
   redirect_uri: 'https://intercom-login-example.netlify.com/.netlify/functions/auth-callback',
-  app_id: process.env.INTERCOM_APP_ID,
 };
 
-const oauth2 = simpleOauth.create({
-  site: ' ',
-  clientSecret: config.clientSecret,
-  clientID: config.clientID,
-  authorizationPath: config.authorizationPath,
-  tokenPath: config.tokenPath,
-});
+const credentials = {
+  client: {
+    id: config.clientID,
+    secret: config.clientSecret
+  },
+  auth: {
+    tokenHost: config.authorizationPath,
+    tokenPath: config.tokenPath,
+  }
+};
+
+const oauth2 = simpleOauth.create(credentials);
 
 export default oauth2
